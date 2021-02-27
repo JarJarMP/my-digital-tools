189ea915c0307cf74c36c1a9484d9870b3b6049a
PeterMaroti <noemail@dummy.com>
2021-02-27 07:36:00 +0100

    update - App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders starter content', () => {
+test('renders without crashing', () => {
   render(<App />);
-  const element = screen.getByText(/MDT/);
-  expect(element).toBeInTheDocument();
 });
