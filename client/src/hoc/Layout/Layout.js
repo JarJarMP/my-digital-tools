e68ebbb183bdedccb8e481d1cdf5a93bf0424e0a
PeterMaroti <noemail@dummy.com>
2021-03-06 08:19:51 +0100

    add - semantic UI to project

--- client/src/hoc/Layout/Layout.js
+++ client/src/hoc/Layout/Layout.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react'
 
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Content from '../../components/Content/Content';
 
 const layout = () => (
-  <React.Fragment>
-    <Sidebar/>
-    <Content/>
-  </React.Fragment>
+  <Grid>
+    <Grid.Row columns={2}>
+      <Grid.Column width={4}>
+        <Sidebar />
+      </Grid.Column>
+      <Grid.Column width={12}>
+        <Content />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 );
 
 export default layout;
